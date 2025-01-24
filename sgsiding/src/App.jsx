6c6e06547c49e69dee55b85ec8405756c7b5266b
Clint <clinthiles1992@gmail.com>
2025-01-24 12:20:19 -0800

    Refactor App component and remove unused CSS for animated background

--- sgsiding/src/App.jsx
+++ sgsiding/src/App.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import "./App.css";
 import { Button } from "./components/ui/button";
-// import { NavbarWithMegaMenu } from "./components/ui/Navbar";
 import Navbar from "./components/ui/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* <NavbarWithMegaMenu /> */}
       <Button onClick={() => console.log("Clicked")}>Click me</Button>
     </>
   );
 }
 
 export default App;
+
+// import React from "react";
+// import "./App.css";
+// import { Button } from "./components/ui/button";
+// // import { NavbarWithMegaMenu } from "./components/ui/Navbar";
+// import Navbar from "./components/ui/Navbar";
+
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+//       {/* <NavbarWithMegaMenu /> */}
+//       <Button onClick={() => console.log("Clicked")}>Click me</Button>
+//     </>
+//   );
+// }
+
+// export default App;
