99635f9d5f1cd04f8a604448e3548f9e8095e4ff
Clint <clinthiles1992@gmail.com>
2025-01-24 16:09:38 -0800

    Refactor Siding component layout and styles for improved responsiveness and structure

--- sgsiding/src/components/ui/Siding.jsx
+++ sgsiding/src/components/ui/Siding.jsx
@@ -21,20 +21,34 @@ export default function Siding() {
   ];
 
   return (
-    <div className="carousel-items">
-      <Carousel className="w-full max-w-5xl mx-auto">
-        <CarouselContent>
-          {slides.map((slide) => (
-            <CarouselSlide
-              key={slide.id}
-              slide={slide}
-            />
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
-    </div>
+    <main className="main-container">
+      <span className="text-content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi
+        dolor, consequatur neque sunt vero? Sed architecto labore officiis
+        doloremque odit! Quas exercitationem sed distinctio molestiae at quam
+        enim nobis. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Odio accusantium dolorum reiciendis expedita ullam hic. Sequi molestias
+        cum accusantium dolor officiis minima explicabo. Dolorum, distinctio
+        aut? Praesentium modi nisi alias. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Repellat repellendus sequi quia facere nam ratione
+        unde culpa deleniti impedit placeat, sit exercitationem facilis
+        perspiciatis porro consequuntur dolore eos numquam minus!
+      </span>
+      <div className="carousel-items">
+        <Carousel className="w-full max-w-5xl mx-auto">
+          <CarouselContent>
+            {slides.map((slide) => (
+              <CarouselSlide
+                key={slide.id}
+                slide={slide}
+              />
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+    </main>
   );
 }
 
