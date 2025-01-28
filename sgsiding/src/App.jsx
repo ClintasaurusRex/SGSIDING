1ebd01acc4abad8b2fab2adeb23c30ae62b281cc
Clint <clinthiles1992@gmail.com>
2025-01-28 09:52:33 -0800

    Update App styling with wood-inspired background and remove unused Siding import

--- sgsiding/src/App.jsx
+++ sgsiding/src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/ui/Navbar";
-import Siding from "./components/ui/Siding";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import About from "./pages/About";
