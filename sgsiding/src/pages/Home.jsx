25de3f623dbac22df95240bad96b9b3532b79bb3
Clint <clinthiles1992@gmail.com>
2025-01-28 10:40:20 -0800

    Add ladder div to Home page with About CSS import

--- sgsiding/src/pages/Home.jsx
+++ sgsiding/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Siding from "../components/ui/Siding";
-
+import "../components/styles/About.css";
 export default function Home() {
   return (
     <div>
-      <Siding />
+      <div className="ladder"></div>
+      <Siding /> 
     </div>
+    
   );
 } 
