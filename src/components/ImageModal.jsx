15202c559d757e6887a43dafdec2142ea485c320
Clint <clinthiles1992@gmail.com>
2025-02-20 15:26:01 -0800

    Add ImageModal component with styling for overlay and content

---
+++ src/components/ImageModal.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/ImageModal.css";
+const ImageModal = ({ src, alt, onClose }) => {
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content">
+        <img src={src} alt={alt} />
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+};
+
+export default ImageModal;
