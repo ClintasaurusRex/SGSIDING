a54256228d93e52497c5c3ac1dc5ec9cb59af295
Clint <clinthiles1992@gmail.com>
2025-03-21 14:19:18 -0700

    fixed up the navbar

--- sgsiding/src/components/NavBarItems.jsx
+++ sgsiding/src/components/NavBarItems.jsx
@@ -1,41 +1,20 @@
-import { cn } from "@/lib/utils";
-import { Link } from "react-router-dom";
-import "./styles/NavBar.css";
+import { Link } from 'react-router-dom';
+import './styles/NavBar.css';
 
 export default function NavBarItems() {
   return (
-    <div className="nav-items">
-      <nav className="hidden md:flex space-x-6 bg-444445">
-        <Link
-          to="/"
-          className={cn(
-            "text-xl font-medium text-muted hover:text-primary link-btns"
-          )}
-        >
+    <div className='nav-items'>
+      <nav className='hidden md:flex space-x-6'>
+        <Link to='/' className='nav-link'>
           Home
         </Link>
-        <Link
-          to="/services"
-          className={cn(
-            "text-xl font-medium text-muted hover:text-primary link-btns"
-          )}
-        >
+        <Link to='/services' className='nav-link'>
           Services
         </Link>
-        <Link
-          to="/about"
-          className={cn(
-            "text-xl font-medium text-muted hover:text-primary link-btns"
-          )}
-        >
+        <Link to='/about' className='nav-link'>
           About Us
         </Link>
-        <Link
-          to="/contact"
-          className={cn(
-            "text-xl font-medium text-muted hover:text-primary link-btns"
-          )}
-        >
+        <Link to='/contact' className='nav-link'>
           Contact
         </Link>
       </nav>
--- sgsiding/src/components/ui/Navbar.jsx
+++ sgsiding/src/components/ui/Navbar.jsx
@@ -1,10 +1,10 @@
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
-import { useState } from "react";
-import { Menu, X } from "lucide-react";
-import { Link } from "react-router-dom";
-import NavBarItems from "../NavBarItems";
-import "../styles/NavBar.css";
+import { useState } from 'react';
+import { Menu, X } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import NavBarItems from '../NavBarItems';
+import '../styles/NavBar.css';
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -12,73 +12,51 @@ const Navbar = () => {
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
-    <header className="bg-444445 border-b shadow-sm transition-all duration-300 hover:shadow-xl">
-      <div className="container mx-auto flex items-center justify-between px-4 py-3">
+    <header className='wood-navbar'>
+      <div className='container mx-auto flex items-center justify-between px-8 py-3'>
         {/* Logo */}
-        <div className="text-lg font-bold text-primary business-title">
+        <div className='text-lg font-bold text-primary business-title'>
           <img
-            src="sglogo.png"
+            src='sglogo.png'
             width={100}
             height={100}
-            alt="SG Siding Logo"
-            className="logo"
+            alt='SG Siding Logo'
+            className='logo'
           />
-          <h2 className="nav-title">Custom Exteriors</h2>
+          <h2 style={{ textDecoration: 'underline', color: 'black' }}>
+            S.G Custom Interiors
+          </h2>
         </div>
 
         {/* Desktop Navigation */}
-        <div className="hidden lg:block">
+        <div className='hidden lg:block'>
           <NavBarItems />
         </div>
 
         {/* Mobile Menu Button */}
         <button
-          className="lg:hidden text-muted hover:text-primary bg-gray-600"
+          className='lg:hidden text-muted hover:text-primary wood-button'
           onClick={toggleMenu}
         >
-          <div className="flex items-center justify-center">
+          <div className='flex items-center justify-center'>
             {menuOpen ? <X size={24} /> : <Menu size={24} />}
           </div>
         </button>
 
         {/* Mobile Navigation */}
         {menuOpen && (
-          <nav className="absolute top-16 left-0 w-full shadow-md border-t lg:hidden nav-btns">
-            <div className="flex flex-col items-center space-y-4 p-4  ">
-              <Link
-                to="/"
-                className={cn(
-                  "text-sm font-medium text-muted hover:text-primary btn-nav"
-                )}
-                onClick={toggleMenu}
-              >
+          <nav className='wood-mobile-nav'>
+            <div className='flex flex-col items-center space-y-4 p-4'>
+              <Link to='/' className='nav-link' onClick={toggleMenu}>
                 Home
               </Link>
-              <Link
-                to="/services"
-                className={cn(
-                  "text-sm font-medium text-muted hover:text-primary btn-nav"
-                )}
-                onClick={toggleMenu}
-              >
+              <Link to='/services' className='nav-link' onClick={toggleMenu}>
                 Services
               </Link>
-              <Link
-                to="/about"
-                className={cn(
-                  "text-sm font-medium text-muted hover:text-primary btn-nav"
-                )}
-                onClick={toggleMenu}
-              >
+              <Link to='/about' className='nav-link' onClick={toggleMenu}>
                 About Us
               </Link>
-              <Link
-                to="/contact"
-                className={cn(
-                  "text-sm font-medium text-muted hover:text-primary btn-nav"
-                )}
-                onClick={toggleMenu}
-              >
+              <Link to='/contact' className='nav-link' onClick={toggleMenu}>
                 Contact
               </Link>
             </div>
